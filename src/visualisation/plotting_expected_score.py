d8061027a54f6f4d1ad575cb7b65ad830b74a826
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2023-12-31 19:50:08 +1100

    Updating expected score plots for report

--- src/visualisation/plotting_expected_score.py
+++ src/visualisation/plotting_expected_score.py
@@ -52,56 +52,31 @@ def get_shot_data_dict(chains, match_id, team):
     
     return shot_data_dict
 
-def plot_expected_score_map(pitch, ax, shot_data_dict, cmap, size_ratio = 3):
+def plot_expected_score_map(pitch, ax, shot_data_dict, cmap, size_ratio = 3, ec = "black"):
 
     norm = plt.Normalize(vmin=0, vmax=6)
     
     pitch.scatter(shot_data_dict['set_misses']['x'], shot_data_dict['set_misses']['y'], ax=ax, s=(shot_data_dict['set_misses']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['set_misses']['xScore'])), alpha=0.3, marker="s")
     pitch.scatter(shot_data_dict['set_behinds']['x'], shot_data_dict['set_behinds']['y'], ax=ax, s=(shot_data_dict['set_behinds']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['set_behinds']['xScore'])), marker="s")
-    pitch.scatter(shot_data_dict['set_goals']['x'], shot_data_dict['set_goals']['y'], ax=ax, s=(shot_data_dict['set_goals']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['set_goals']['xScore'])), ec="white", marker="s")
+    pitch.scatter(shot_data_dict['set_goals']['x'], shot_data_dict['set_goals']['y'], ax=ax, s=(shot_data_dict['set_goals']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['set_goals']['xScore'])), ec=ec, marker="s")
 
     pitch.scatter(shot_data_dict['open_misses']['x'], shot_data_dict['open_misses']['y'], ax=ax, s=(shot_data_dict['open_misses']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['open_misses']['xScore'])), alpha=0.3)
     pitch.scatter(shot_data_dict['open_behinds']['x'], shot_data_dict['open_behinds']['y'], ax=ax, s=(shot_data_dict['open_behinds']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['open_behinds']['xScore'])))
-    pitch.scatter(shot_data_dict['open_goals']['x'], shot_data_dict['open_goals']['y'], ax=ax, s=(shot_data_dict['open_goals']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['open_goals']['xScore'])), ec="white")
+    pitch.scatter(shot_data_dict['open_goals']['x'], shot_data_dict['open_goals']['y'], ax=ax, s=(shot_data_dict['open_goals']['xScore']**2)*size_ratio, c=cmap(norm(shot_data_dict['open_goals']['xScore'])), ec=ec)
     
     return pitch, ax
 
-def add_vertical_shot_map_legend(fig, ax, cmap, size_ratio = 3):
+def calculate_total_team_expected_score(shot_data_dict):
     
-    norm = plt.Normalize(vmin=0, vmax=6)
-    
-    ## Manual Legend
-    legend_ax = fig.add_axes([0.72, 0.4, 0.2, 0.3])
-    legend_ax.axis("off")
-    plt.xlim([0, 7.1])
-    plt.ylim([0, 1.1])
-    legend_ax.xaxis.set_tick_params(color="white")
-    for size in [1, 2, 3, 4, 5, 6]:
-        legend_ax.scatter(size, 0.95, s=(size**2)*size_ratio, c=cmap(norm(size)))
-    for size in [1, 6]:
-        legend_ax.text(size-0.14, 0.82, str(size), color="white", fontsize=8, font='Karla')
-        
-    legend_ax.scatter(3.5, 0.65, s=(4**2)*size_ratio, c=cmap(norm(4)), ec="white")
-    legend_ax.scatter(3.5, 0.53, s=(4**2)*size_ratio, c=cmap(norm(4)))
-    legend_ax.scatter(3.5, 0.41, s=(4**2)*size_ratio, c=cmap(norm(4)), alpha=0.3)
-    legend_ax.scatter(5, 0.65, s=(4**2)*size_ratio, c=cmap(norm(4)), ec="white", marker="s")
-    legend_ax.scatter(5, 0.53, s=(4**2)*size_ratio, c=cmap(norm(4)), marker = "s")
-    legend_ax.scatter(5, 0.41, s=(4**2)*size_ratio, c=cmap(norm(4)), alpha=0.3, marker = "s")
+    return sum([shot_data_dict[shot_type]['xScore'].sum() for shot_type in shot_data_dict.keys()])
 
-    legend_ax.text(2.8, 0.73, "Open", color="white", fontsize=8, font='Karla')
-    legend_ax.text(4.5, 0.73, "Set", color="white", fontsize=8, font='Karla')
-    legend_ax.text(1, 0.62, "Goal", color="white", fontsize=8, font='Karla')
-    legend_ax.text(1, 0.5, "Behind", color="white", fontsize=8, font='Karla')
-    legend_ax.text(1, 0.38, "Miss", color="white", fontsize=8, font='Karla')
-    
-    return ax
-
-def plot_vertical_pitch_team_expected_score(ax, chain_data, match_id, team):
+def plot_vertical_pitch_team_expected_score(ax, chain_data, match_id, team, fontsize = 36, size_ratio=3, ec="black"):
     
     shot_data_dict = get_shot_data_dict(chain_data, match_id, team)
     pitch, ax = plot_half_vertical_pitch_ax(ax)
-    pitch, ax = plot_expected_score_map(pitch, ax, shot_data_dict, cmap = team_colourmaps[team], size_ratio = 3)
+    pitch, ax = plot_expected_score_map(pitch, ax, shot_data_dict, cmap = team_colourmaps[team], size_ratio = size_ratio, ec=ec)
     
-    ax = add_ax_text(ax, 'Expected Score Shot Map', team=team)
+    team_expected_score = calculate_total_team_expected_score(shot_data_dict)
+    ax = add_ax_text(ax, f'{team_expected_score:.1f}', team=team, fontsize=fontsize)
     
     return ax
